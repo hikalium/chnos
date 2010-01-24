44e477278095c21b01a9218e835547ecfb75163e
hikarupsp <hikarupsp@447e56a9-82e3-4d55-9dd4-5b2b0d47896b>
2010-01-24 08:42:13 +0000

    git-svn-id: https://svn.osdn.jp/svnroot/chnosproject/beta@32 447e56a9-82e3-4d55-9dd4-5b2b0d47896b

--- tolset_chn_000/chnos_003/bootpack.c
+++ tolset_chn_000/chnos_003/bootpack.c
@@ -5,13 +5,17 @@ void boxfill16(unsigned short *vram, int xsize, unsigned short c, int x0, int y0
 void init_scrn(unsigned short *vram, int xsize, int ysize);
 void putfonts16_asc(unsigned short *vram, int xsize, int x, int y, unsigned short c, unsigned char *s);
 void putfont16(unsigned short *vram, int xsize, int x, int y, unsigned short c, char *font);
-
+void init_mouse_cursor16(short *mouse, unsigned short bc);
+void putblock16_16(unsigned short *vram, int vxsize, int pxsize,int pysize, int px0, int py0, short *buf, int bxsize);
 
 void CHNMain(void)
 {	
+	unsigned short mousecur[1024];
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	struct VESAINFO *vinfo = (struct VESAINFO *) 0x0e00;
 	init_scrn(vinfo->PhysBasePtr, binfo->scrnx, binfo->scrny);
+	init_mouse_cursor16(mousecur, RGB16(5,10,5));
+	putblock16_16(vinfo->PhysBasePtr, binfo->scrnx, 32, 32, binfo->scrnx/2, binfo->scrny/2, mousecur, 32);
 	for(;;) {
 	io_hlt();
 	}
@@ -22,8 +26,9 @@ void init_scrn(unsigned short *vram, int xsize, int ysize )
 {
 	boxfill16(vram,xsize,RGB16(5,10,5),0,0,xsize,ysize);
 	boxfill16(vram,xsize,RGB16(20,40,30),0,ysize-40,xsize,ysize);
+	boxfill16(vram,xsize,RGB16(20,40,30),xsize/2-8,ysize/2-8,xsize/2+8,ysize/2+8);
 	putfonts16_asc(vram, xsize, 8, 8, RGB16(31,62,31), "welcome to CHNOSProject!");
-
+	return;
 
 }
 
@@ -67,3 +72,69 @@ void putfont16(unsigned short *vram, int xsize, int x, int y, unsigned short c, 
 	}
 	return;
 }
+
+void init_mouse_cursor16(short *mouse, unsigned short bc)
+{
+	static char cursor[32][32] = {
+		"***.............................",
+		"*O**............................",
+		"*OO**...........................",
+		"*OOO**..........................",
+		"*OOOO**.........................",
+		"*OOOOO**........................",
+		"*OOOOOO**.......................",
+		"*OOOOOOO**......................",
+		"*OOOOOOOO**.....................",
+		"*OOOOOOOOO**....................",
+		"*OOOOOOOOOO**...................",
+		"*OOOOOOOOOOO**..................",
+		"*OOOOOOOOOOOO**.................",
+		"*OOOOOOOOOOOOO**................",
+		"*OOOOOOOOOOOOOO**...............",
+		"*OOOOOOOOOOOOOOO**..............",
+		"*OOOOOOOOOOOOOOOO**.............",
+		"*OOOOOOOOOOOOOOOOO**............",
+		"*OOOOOOOOOOOOOOOOOO**...........",
+		"*OOOOOOOOOOOOOOOOOOO**..........",
+		"*OOOOOOOOOO************.........",
+		"*OOOOOOOOOO*....................",
+		"*OOOOOOOOOOO*...................",
+		"*OOOOOOO*OOO*...................",
+		"*OOOOOO**OOOO*..................",
+		"*OOOOO**.*OOO*..................",
+		"*OOOO**..*OOOO*.................",
+		"*OOO**....*OOO*.................",
+		"*OO**.....*OOO*.................",
+		"*O**.......***..................",
+		"***.............................",
+		"**.............................."
+	};
+	int x, y;
+
+	for (y = 0; y < 32; y++) {
+		for (x = 0; x < 32; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 32 + x] = RGB16(0,0,0);
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 32 + x] = RGB16(31,62,31);
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 32 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+void putblock16_16(unsigned short *vram, int vxsize, int pxsize,int pysize, int px0, int py0, short *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
+}
+
