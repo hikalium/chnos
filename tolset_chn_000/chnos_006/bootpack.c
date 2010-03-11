c7d64b9753eb8125444f5c8a0dfd1b1277ce41c1
hikarupsp <hikarupsp@447e56a9-82e3-4d55-9dd4-5b2b0d47896b>
2010-03-11 14:44:13 +0000

    mouseid取得可能
    git-svn-id: https://svn.osdn.jp/svnroot/chnosproject/beta@66 447e56a9-82e3-4d55-9dd4-5b2b0d47896b

--- tolset_chn_000/chnos_006/bootpack.c
+++ tolset_chn_000/chnos_006/bootpack.c
@@ -2,7 +2,7 @@
 
 void CHNMain(void)
 {	
-	unsigned char s[24] , mdatabuf[3], m_phase = 0;
+	unsigned char s[24] , mdatabuf[3], m_phase = 0,m_init = 0, m_id = 0;
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	struct VESAINFO *vinfo = (struct VESAINFO *) ADR_VESAINFO;
 	struct FIFO32 sysfifo;
@@ -39,7 +39,71 @@ void CHNMain(void)
 			io_sti();
 			switch (m_phase) {
 				case 0:
-					if(i == 0xfa) { m_phase = 1; }
+					if(i == 0xfa) { 
+						switch (m_init) {
+							case 0:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 0xf3);
+								m_init = 1;
+								break;
+							case 1:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 200);
+								m_init = 2;
+								break;
+							case 2:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 0xf3);
+								m_init = 3;
+								break;
+							case 3:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 100);
+								m_init = 4;
+								break;
+							case 4:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 0xf3);
+								m_init = 5;
+								break;
+							case 5:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 0x80);
+								m_init = 6;
+								break;
+							case 6:
+								wait_KBC_sendready();
+								io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+								wait_KBC_sendready();
+								io_out8(KEYB_DATA, 0xf2);
+								m_init = 7;
+								break;
+							case 7:
+								wait_KBC_sendready();
+								m_init = 8;
+								break;
+							case 8:
+								wait_KBC_sendready();
+								m_id = io_in8(KEYB_DATA);
+								m_phase = 1;
+								boxfill_i(vinfo->PhysBasePtr, binfo->scrnx, 0x000000, 0,272,300 , 288);	
+								sprintf(s,"Ï³½ID = %02X",m_id);
+								putfonts_asc_i(vinfo->PhysBasePtr, binfo->scrnx, 0,272,0xffffff,s);	
+								break;
+						}
+					 }
 					break;
 				case 1:
 					mdatabuf[0] = i;
